3f8f264d66706d8b76266962ffccc215272eeb52
Roman Gardukevich <roman@gardukevich.com>
2025-06-07 22:13:26 +0300

    Initial Commit

---
+++ FindH264/FindH264App.swift
@@ -0,0 +1,17 @@
+//
+//  FindH264App.swift
+//  FindH264
+//
+//  Created by odnairy on 7.6.2025.
+//
+
+import SwiftUI
+
+@main
+struct FindH264App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
